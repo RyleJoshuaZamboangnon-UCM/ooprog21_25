002155abd647878e17d4a1039d6dbacf8774969d
RyleJoshuaZamboangnon-UCM <ryleworkacc@gmail.com>
2025-08-29 18:41:00 +0800

    Add files via upload

---
+++ Chapter_1/TableAndChairs.java
@@ -0,0 +1,21 @@
+
+
+
+    public class TableAndChairs {
+        
+            public static void main(String args []) {
+              
+                   back(4);
+                   System.out.print("X      XXXXXXXXXX      X");
+                   System.out.println("\nXXXXX  X        X  XXXXX");
+                   for (int Legs = 1; Legs < 3; Legs++) {
+                          System.out.println("X   X  X        X  X   X");
+            }
+            }
+
+            public static void back(int back) {
+                   System.out.println("X\t\t       X");
+                   System.out.println("X\t\t       X");
+            }
+            }
+        
