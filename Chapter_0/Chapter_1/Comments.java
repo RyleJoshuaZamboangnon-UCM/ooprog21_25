15b5681d051ab2d176a96734dc207dfc5b91359d
RyleJoshuaZamboangnon-UCM <ryleworkacc@gmail.com>
2025-08-29 18:39:28 +0800

    Add files via upload

---
+++ Chapter_0/Chapter_1/Comments.java
@@ -0,0 +1,28 @@
+
+
+   
+
+
+
+    public class Comments {
+    
+             public static void main(String args[]) {
+                
+                // 1.  Line Comment.
+              
+                /* 2.
+                 Block
+                  Comment
+                  */
+                
+                /** 3.
+                 * 
+                 * Documentation
+                 * Comment
+                 */
+                
+               
+                
+             }
+             
+    }
