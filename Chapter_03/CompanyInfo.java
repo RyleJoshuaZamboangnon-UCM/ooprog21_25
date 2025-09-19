b36e1d15c3a07e52fd6bce8adc30372c72515d3b
RyleJoshuaZamboangnon-UCM <ryleworkacc@gmail.com>
2025-09-19 08:28:04 +0800

    Create CompanyInfo.java

---
+++ Chapter_03/CompanyInfo.java
@@ -0,0 +1,15 @@
+public class CompanyInfo{
+   //main method
+   public static void main(String [] a){
+   //company name
+      System.out.println("Company name:Smart Electronics");
+      
+      displayHours();
+   }
+   //method
+   public static void displayHours(){
+      System.out.println("Monday - Friday 8 am to 6 pm");
+      System.out.println("Saturday 8 am to noon");
+      System.out.println("Sunday closed");
+   }
+}
